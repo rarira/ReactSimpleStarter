b689d0dee208e73a9df5471d50e82b7df82c7822
Inseong Park <rarira@gmail.com>
2017-10-29 21:46:02 +0900

    28 finished

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyCW5Src9sAmOe-_gCXhmZULbFxXKmuzFu0';
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = { videos:[] };
+
     YTSearch({key:API_KEY, term:'surfboards'},(videos) => {
       this.setState({videos});
     });
@@ -20,10 +22,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
   }
-}
+};
 
 ReactDOM.render(<App />,document.querySelector('.container'));
